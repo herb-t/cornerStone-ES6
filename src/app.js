b1a4689d7a6b98857c29f6c623d77f1dd060072b
Herb Torres <herb.torres@byhook.com>
2017-10-13 15:06:51 -0400

    i'm pretty...oh so pretty

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-import config from './config';
+import config from "./config";
 
 let testAppJs = () => {
-  console.log('app.js imported to index.js');
+  console.log("app.js imported to index.js");
   console.log(config);
-
 };
 
 export default testAppJs;
